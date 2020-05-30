9c49481600d55f4361a7a1e37f88076dc8921931
sarathchandra706 <sarathchandra706@gmail.com>
2020-05-30 17:53:10 +0530

    seven multiples

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function isSevenMultiple(num) {
+    if ((num > 0) && (num % 7 == 0)){
+        return true;
+    }
+     return false;
+    
+}
 
+
+var assert=require('assert') 
+assert(isSevenMultiple(35),"Testcase1 passed");
+assert(isSevenMultiple(12)===false,"Testcase2 passed");
+assert(isSevenMultiple(40)===false,"Testcase3 passed");
+assert(isSevenMultiple(220)=== false,"Testcase4 passed");
+console.log("All testcases passed");
