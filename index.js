163315965089bdb3ca8855f7db988bc9983cdcc2
srikar-1997 <thalladasrikar7@msitprogram.net>
2020-05-30 18:14:05 +0530

    srikar

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function isEven(no) {
+    if (no % 2 === 0) {
+        return true;
+    }
+    return false;
+}
 
+var assert = require('assert');
+
+assert(isEven(2), 'test case 1 passed');
+assert(isEven(3) === false, 'test case 2 passed');
+assert(isEven(4), 'test case 3 passed');
+assert(isEven(5) === false, 'test case 4 passed');
+
+console.log("all test cases passed")
