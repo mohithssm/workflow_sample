98dc141edeb8b9acea0d5cfdd1310e9b847bffeb
Hima-Bindu97 <himabindureddygotike@msitprogram.net>
2020-05-30 17:54:50 +0530

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-
+function armstrong(x) {
+    var a=x,b,sum=0;
+    while(a>0) {
+        b=a%10;
+        sum+=(b*b*b);
+        a=parseInt(a/10);
+    }
+    if (sum===x) {
+        return "armstrong number";
+    } else {
+        return "not an armstrong number";
+    }
+}
+var assert=require('assert')
+assert(armstrong(153),"testcase passed");
+assert(armstrong(5)===false,"testcase2 passed");
+console.log("all testcases passed");
