32c2604837c666574878430566ddc335b3df83db
lakshmisindhu9100733271 <lakshmisindhu996@msitprogram.net>
2020-05-30 17:47:37 +0530

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+function leapyear(year)
+{
+return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+}
+
+var assert=require('assert') 
+assert(leapyear(2016),"Testcase1 passed");
+assert(leapyear(1700)===false,"Testcase2 passed");
+assert(leapyear(100)===false,"Testcase3 passed");
+assert(leapyear(1800)===false,"Testcase4 passed");
+console.log("All testcases passed");
