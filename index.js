cf661384445127c9364d08ec013d52b15938eeac
akankshadhanewar <55694846+akankshadhanewar@users.noreply.github.com>
2020-05-30 19:24:17 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+function isPalindrome(input){
+    if(typeof input === 'string'){
+        var input = input.toLowerCase().replace(/[^a-zA-Z0-9]+/g,'');
+    }
+    var c = 0;
+    if(input==="") {
+        return false;
+    }
+    if((input.length) % 2 === 0) {
+        c = (input.length) / 2;
+    } 
+    else {
+        if (input.length === 1) {
+            return true;
+        } 
+        else {
+            c = (input.length - 1) / 2;
+        }
+    }
+    for (var x = 0; x < c; x++) {
+        if (input[x] != input.slice(-1-x)[0]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+var assert = require("assert");
+assert(isPalindrome(101) == true);
+assert(isPalindrome(10101010101) == true);
+assert(isPalindrome("Madam") == true);
+assert(isPalindrome("Hai") == false);
+assert(isPalindrome(10000000002001) == false);
+assert(isPalindrome("Hello") == false);
+console.log("All test cases passed");
